fdccdbead41d49cab16cbae4917033b375646d2c
someone5678 <nemui3353@gmail.com>
2022-06-16 08:38:33 -0300

    veux: prebuilts: Add CameraGo v3.3.439217877
    - Official release 3.3.439217877
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -112,6 +112,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    CameraGo \
+    privapp-permissions-camera-go.xml \
     libcamera2ndk_vendor \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
