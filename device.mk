1864b3669acc8e62536f3f4e68a102bb62f1c4c4
SharmagRit <sharmagrit04@gmail.com>
2022-07-26 21:49:52 +0530

    veux: Import devicesettings * From: https://github.com/ArrowOS-Devices/android_device_xiaomi_sweet.git
    Signed-off-by: SharmagRit <sharmagrit04@gmail.com>

--- device.mk
+++ device.mk
@@ -132,6 +132,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
