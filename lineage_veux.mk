37b0a95d5e83f4448ac8849c2fd13a5a1f8fd98b
SharmagRit <sharmagrit04@gmail.com>
2022-09-08 16:55:52 +0530

    veux: It's riceDroid time

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -8,21 +8,35 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
+# Inherit some common riceDroid Stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_veux
 PRODUCT_DEVICE := veux
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X4 Pro 5G
+PRODUCT_MANUFACTURER := Xiaomi
+
+# riceDroid Stuff with GApps
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+SUSHI_BOOTANIMATION  := 1080
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="veux-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/veux_global/veux:11/RKQ1.211001.001/V13.0.10.0.RKCMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/veux_p_global/veux:11/RKQ1.211001.001/V13.0.10.0.RKCMIXM:user/release-keys
+
+# Maintainer Stuff
+RICE_MAINTAINER := SharmaG
+RICE_OFFICIAL := true
