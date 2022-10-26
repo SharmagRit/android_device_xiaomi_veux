99bd434504785fd6c3cdf9bb7f0a22ea0607e15b
SharmagRit <sharmagrit04@gmail.com>
2022-10-26 19:23:52 +0530

    veux: Drop DeviceSettings

--- device.mk
+++ device.mk
@@ -135,10 +135,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
